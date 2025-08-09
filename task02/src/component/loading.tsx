4bef39741b632a54e1a0f1f085bf0fea316051c4
excel <axceldesigns@gmail.com>
2025-08-09 13:40:33 +0100

    task

---
+++ task02/src/component/loading.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Loading() {
+  return (
+    <div className="flex justiy-around bg-red-700">
+      <h1 className="text-white font-bold">Loading...</h1>
+    </div>
+  );
+}
