e379d6a5e8c8d53688598c076981b31b9533c6f5
excel <axceldesigns@gmail.com>
2025-08-12 04:37:16 +0100

    task

--- task02/src/component/AddPost.tsx
+++ task02/src/component/AddPost.tsx
@@ -1,5 +1,13 @@
 
 export default function AddPost() {
+  const data = {
+    "createdAt": "2025-08-10T03:06:20.176Z",
+    "name": "Wesley Gibson",
+    "avatar": "https://cdn.jsdelivr.net/gh/faker-js/assets-person-portrait/female/512/8.jpg",
+    "title": "Ratione ullam campana.",
+
+  }
+
   return (
     <div>AddPost</div>
   )
